195639c15a9244bea0b55f2454f322a9bc541630
Paul Lee <chaiyapong@3dsinteractive.com>
2017-11-04 08:02:24 +0700

    Rename script to ps1.

--- workshops/07_PhpUnit/laravel-quickstart/composer-update.bat
+++
@@ -1,3 +0,0 @@
-IF EXIST %cd%\web\vendor docker run -i --rm -v "%cd%\web:/app" 3dsinteractive/composer:7.1 update
-IF NOT EXIST %cd%\web\vendor docker run -i --rm -v "%cd%\web:/app" 3dsinteractive/composer:7.1 install
-cls
